89112a58fd90e0f03fd1d09f348dd77cb8280d9a
RifatHossaN <rifathosan098@gmail.com>
2025-01-28 00:26:18 +0600

    added user name into the post

--- resources/views/components/postCard.blade.php
+++ resources/views/components/postCard.blade.php
@@ -3,7 +3,7 @@
 <div class="bg-cyan-950 p-4 m-2 text-white flex flex-col gap-3 rounded-lg">
     <div>
         <h3 class="text-base font-semibold">{{$post->title}}</h3>
-        <h3 class="text-xs">Posted {{$post->created_at->diffForHumans()}} by </h3>
+        <h3 class="text-xs">Posted {{$post->created_at->diffForHumans()}} by {{$post->user->username}}</h3>
     </div>
     
     <p class="text-sm">{{Str::words($post->body,15,"")}}<b class="font-semibold"> ...See More</b></p>

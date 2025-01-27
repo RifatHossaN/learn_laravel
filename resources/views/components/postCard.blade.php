59092850aaed9e58ddd8a2691acc32c39420bb24
RifatHossaN <rifathosan098@gmail.com>
2025-01-28 01:57:22 +0600

    showing indivitual post using show method under dashboard controller

--- resources/views/components/postCard.blade.php
+++ resources/views/components/postCard.blade.php
@@ -1,11 +1,20 @@
-@props(['post'])
+@props(['post', 'full'=>false])
 
 <div class="bg-cyan-950 p-4 m-2 text-white flex flex-col gap-3 rounded-lg">
     <div>
         <h3 class="text-base font-semibold">{{$post->title}}</h3>
         <h3 class="text-xs">Posted {{$post->created_at->diffForHumans()}} by <a class="text-blue-400" href="{{route('user.posts',$post->user)}}">{{$post->user->username}}</a></h3>
     </div>
+
     
-    <p class="text-sm">{{Str::words($post->body,15,"")}}<b class="font-semibold"> ...See More</b></p>
+    @if ($full)
+        <p class="text-sm">{{$post->body}}</p>
+    @else
+        <p class="text-sm">{{Str::words($post->body,15,"")}}<b class="font-semibold"> <a class="text-blue-500" href="{{route('post.show',$post)}}">...See More</a></b></p>
+    @endif
+
+    
+    
+    
      
 </div>
